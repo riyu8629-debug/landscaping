bbaabc58751aa3656907d590e4f7c505efb4b56b
Riya <riyaqureshi661@gmail.com>
2025-11-15 15:03:19 +0530

    this is fist commit of my project;

---
+++ app.js
@@ -0,0 +1,444 @@
+// let a = 3;
+// let x = (100 + 50) * a;
+// console.log(x);
+
+// let a = 10;
+// let b = 3;
+// console.log(a+b);
+
+// let a = 10;
+// let b = 3;
+// console.log(a-b);
+
+// let a = 10;
+// let b = 3;
+// console.log(a*b);
+
+// let a = 10;
+// let b = 3;
+// console.log(a/b);
+
+// let a = 5;
+// let b = 2;
+// console.log(a/b);
+
+// let a = 10;
+// let b = 3;
+// console.log(a/b);
+
+// let a = 22;
+// let b = 5;
+// console.log(a/b);
+
+// let a = 20;
+// let b = 3;
+// console.log(a/b);
+
+// let a = 22;
+// let b = 5;
+// console.log(a%b);
+
+// let a = 10;
+// let b = 3;
+// console.log(a%b);
+
+// let a = 22;
+// let b = 5;
+// console.log(a/b);
+
+// let a = 22;
+// let b = 5;
+// console.log(a**b);
+
+// let a = 18;
+// let b = 3;
+// console.log(a**b);
+
+// let a = 13;
+// let b = 2;
+// console.log(a**b);
+
+// let x = 10
+// console.log("x:",x);
+
+// let x = 10;
+// x += 5;
+// console.log("x += 5:",x);
+
+// let x = 20;
+// x -= 2;
+// console.log("x -= 2:",x);
+
+// let x = 10;
+// x *= 5;
+// console.log("x *= 2:",x);
+
+// let x = 10;
+// x /= 5;
+// console.log("x /= 5:",x);
+
+// let x = 10;
+// x %= 5;
+// console.log("x %= 5:",x);
+
+// let a = 10;
+// let b = "10";
+// console.log("a == b:", a==b);
+
+// let a = 10;
+// let b = "10";
+// console.log("a === b:", a === b);
+
+// let a = 10;
+// let b = "10";
+// console.log("a != b:", a != b);
+
+// let a = 10;
+// let b = "10";
+// console.log("a !== b:", a !== b);
+
+// let a = 10;
+// let b = "10";
+// console.log("a > 5:", a > 5);
+
+
+// function add  (a,b){
+// return (a+b);
+// }
+// console.log(10+20)
+
+// function add (a){
+//     return(a*a);
+// }
+// console.log(add(5))/
+// suqare"
+
+
+// function great (name){
+//     return (name*name)
+// }
+// let x= great(9);
+
+// console.log(x)
+
+// function great (name){
+//     return ("riya"+name)
+// }
+// console.log(great("qureshi"))
+
+// function number (add){
+//     return(add*add)
+// }
+// console.log(number(5))
+
+// function num (add){
+//     return(add*add*add)
+// }
+// console.log(12*13*14)
+
+
+// function sum(a,b,c){
+//     return(a*b*c)
+// }
+// console.log (sum(1,2,3))
+
+// function a(name){
+//     return("riya"+ "  "+name)
+// }
+// console.log(a("qureshi"))
+
+
+// function areaOfrectangle(length,breath){
+//     return length*breath
+// }
+// console.log(areaOfrectangle(300,400))
+
+// function area(length){
+//     return(length*length)
+// }
+// console.log(area(10,30))
+
+// function test ( ){
+//     console.log("hi")
+// }
+// let h = test()
+// console.log(h)
+
+// function greet(){
+//     console.log("Hello , welcome to js!");
+// }
+// greet()
+
+
+// function add(a,b){
+//     console.log("sum:",a+b);
+// }
+// add(5 ,10);
+// add(20,30);
+
+// function multiply(x,y){
+//     return x * y ;
+// }
+// let result = multiply(4,5);
+// console.log("result:",result)
+
+// function plus(a,b){
+//     return a + b;
+// }
+// let result = plus(8,3)
+// console.log(result)
+
+// function minus(x ,y){
+//     return x-y;
+// }
+// let result = minus(10,3);
+// console.log(result)
+
+// let square = function(n){
+//     return n*n
+// }
+// console.log(square(6));
+
+// let square = function(r){
+//     return r*r;
+// }
+// console.log(square(8))
+
+// function add (a , b){
+//     return a + b;
+// }
+// console.log(add(5,3));
+
+// function subtract (a , b){
+//     return a - b;
+// }
+// console.log(subtract(10 , 4));
+
+// function num (x , y){
+//     return x + y;
+// }
+// console.log(num(10,5));
+
+// function ri(a , b){
+//     return a - b;
+// }
+// console.log(ri(10,9))
+
+// function multiply(a , b){
+//     return a * b;
+// }
+// console.log(multiply(6 , 7))
+
+// function divide (a , b){
+//     return a / b;
+// }
+// console.log(divide(20 , 5));
+
+// function isEven(n){
+//     return n % 2 === 0? "Even" : "odd";
+// }
+// console.log(isEven(7));
+
+// function square(n){
+//     return n * n;
+// }
+// console.log(square(9))
+
+// function cube(n){
+//     return n ** 3
+// }
+// console.log(cube(3));
+
+// function cube (n){
+//     return n ** 5
+// }
+// console.log(cube(5))
+
+// function factorial(n){
+//     let fact = 1;
+//     for (let i =1; i<= n; i++) fact *=i;
+//     return fact;
+// }
+// console.log(factorial(5));
+
+// function maxOfTwo(a , b){
+//     return a < b ? a : b;
+// }
+// console.log(maxOfTwo(10,20));
+
+// function isPrime(n){
+//     if (n < 2) return false;
+//     for (let i = 2; i <= Math.sqrt(n); i++){
+//         if (n % i ===0) return false;
+//     }
+//     return true;
+// }
+// console.log(isPrime(7));
+
+// function sumArray(arr){
+//     return arr.reduce((sum, n) => sum + n,0);
+// }
+// console.log(sumArray([1,2,3,4]));
+
+// function max (n){
+//     return n ** n
+// }
+// console.log(max(5));
+
+
+// function power(x , y){
+//     return x ** y;
+// }
+// console.log(power(2, 3));
+
+
+// function power (a , b){
+//     return a * b;
+// }
+// console.log(power(2,3))
+
+// function printName(name){
+//     console.log("hello," + name + "!")
+// }
+// printName("Riya")
+
+// function fullName(first , last){
+//     return(first + last);
+// }
+// console.log(fullName("riya" + "qureshi"));
+
+// function countLetters(name){
+//     return name.length;
+// }
+// console.log(countLetters("riya"))
+
+
+// function printName(Fname ){
+//     return "Hello," + Fname;
+// }
+// console.log(printName("Riya"))
+
+// function getFullName(first , last){
+//     return first + " " + last;
+// }
+// console.log(getFullName("riya","qureshi"))
+
+
+// function countLetters(name){
+//     return name.length
+// }
+// console.log(countLetters("qureshi"));
+
+// function countLetter(name){
+//     return name.length;
+// }
+// console.log(countLetter("navjot"));
+
+// function fullName(name1 , name2){
+//     return name1 + " " + name2;
+// }
+// console.log(fullName("riya" ,"nav"))
+
+// function reverseName(name){
+//     return name.split("").reverse().join("")
+// }
+// console.log(reverseName("navjot"));
+
+// function sayhello(){
+//     console.log("hello world");
+    
+// }
+// sayhello()
+
+// function printName(name){
+//     console.log("hello,"+name);
+    
+// }
+// printName("riya")
+
+// function addNumber(a , b){
+//     return a + b
+// }
+// console.log(addNumber(10,30))
+
+// function subtractNumber( a , b){
+//     return (a - b);
+// }
+// console.log(subtractNumber(10,5))
+
+// function age (a,b){
+//     return b - a
+// }
+// console.log(age(2002,2025));
+
+
+// ...........Arrow functions ................................................................
+
+// const product=(a,b) =>{
+//     return a * b
+// }
+// console.log(product(2,3));
+
+// const age = (a,b)=>{
+//     return b - a
+// }
+// console.log(age(2002,2025));
+
+// const fullName = (first , last)=> first + " " + last
+//     console.log(fullName("riya", "qureshi"));
+    
+// const fullName =(first , last)=>{
+//     return first + " " + last
+// }
+// console.log(fullName("riya","qureshi"))
+
+// ............................one linear............................................................................................................. 
+
+// const squre = num => num *num
+// console.log(squre(2));
+
+// const add = (num1 ,num2)=>num1 % num2
+// console.log(add(5,2));
+
+// const num = (add1 , add2 ,add3)=> add1 * add2* add3
+// console.log(num(3,4,5));
+
+// const age = (a,b)=>b-a
+// console.log(age(2002,2025));
+
+// ...............................................................................
+
+// const add = (a , b)=> a + b
+// console.log(add(2,3));
+
+// function isEven(num){
+//     return num % 2 ===0;
+// }
+// console.log(isEven(4))
+
+// function issOdd(add){
+//     return add % 4 ===0
+// }
+// console.log(issOdd(3));
+
+// const isEven = num => num % 2 ===0;
+// console.log(isEven(7));
+
+// function greet (name){
+//     return "hello,"+ name + "!"
+// }
+// console.log(greet("Riya"));
+
+// const greet = name =>`hello, ${name}!`
+// console.log(greet("riya"));
+
+// const greet = name => `hello , ${name}`
+// console.log(greet("riya"));
+
+// let name = "riya"
+// let age = 21;
+// console.log(`${name}, ${age} `);
+
+// let a = 8;
+// let b = 12;
+// console.log(`the sum of ${a} and ${b} is ${a + b}`);
